c78373e221ba159d66741596e5082840bc8728c6
xuwei <franklxw@outlook.com>
2023-06-08 18:21:20 +0800

    add samba server and set user passwd

--- hosts/minium/default.nix
+++ hosts/minium/default.nix
@@ -32,6 +32,7 @@
   virtualisation.docker.enable = true;
   users.users.${username} = {
     isNormalUser = true;
+    hashedPassword = "$6$H4uVu105iaTFUNr6$LXi33OjyiRKiY9L4RLVmEIoYYVNLApbbwf/5Q/GtOL.LurJlufYfInoPSrsoFfVa/vqi8Gt8Elu0eOyzxL2WC1";
     extraGroups = [ "docker" "wheel" ];
   };
 
@@ -41,6 +42,20 @@
     settings.PermitRootLogin = "yes";
     settings.PasswordAuthentication = true;
     # I'll disable this once I can connect.
+  };
+  services.samba = {
+    enable = true;
+    shares."nixos" = {
+      path = "home/xuwei";
+      browseable = true;
+      writable = true;
+      printable = false;
+      guestOk = false;
+      createMask = "0644";
+      directoryMask = "0755";
+      validUsers = [ "xuwei" ];
+      writeList = [ "xuwei" ];
+    };
   };
 
   time.timeZone = "Asia/Shanghai";
